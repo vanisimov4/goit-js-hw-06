f46b9dab328deeeff1d4cbeb814cb99cc8e89138
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2023-12-25 15:39:23 +0200

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,22 @@
 'use strict';
-function getShippingMessage(country, price, deliveryFee) {
-    return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+class Storage { 
+    constructor(items) { 
+        this.items = items;
+    }
+    getItems() { 
+        return this.items;
+    }
+    addItem(newItem) { 
+        this.items.push(newItem);
+    }
+    removeItem(itemToRemove) { 
+        this.items = this.items.filter(!itemToRemove);
+    }
 }
- 
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
