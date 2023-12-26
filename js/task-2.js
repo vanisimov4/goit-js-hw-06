f6d97b152acc1a5a9f6012d065feaf62f9c19353
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2023-12-26 15:14:51 +0200

    Done

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,22 @@
 'use strict';
-class Storage { 
-    constructor(items) { 
-        this.items = items;
-    }
-    getItems() { 
-        return this.items;
-    }
-    addItem(newItem) { 
-        this.items.push(newItem);
-    }
-    removeItem(itemToRemove) { 
-        this.items = this.items.filter(!itemToRemove);
-    }
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.items = this.items.filter(item => item != itemToRemove);
+  }
 }
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
