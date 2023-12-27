35fdc70b2d567af1dc3a002eb7f67eae1cab3146
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2023-12-27 10:29:05 +0200

    UpdateAfterReview

--- js/task-1.js
+++ js/task-1.js
@@ -6,20 +6,20 @@ const customer = {
   orders: ['Burger', 'Pizza', 'Salad'],
   // Change code below this line
   getBalance() {
-    return this.balance;
+    return this.balance; // Get current balance
   },
   getDiscount() {
-    return this.discount;
+    return this.discount; // Get current discount
   },
   setDiscount(value) {
-    this.discount = value;
+    this.discount = value; // Set new velue of discount
   },
   getOrders() {
-    return this.orders;
+    return this.orders; // Get currnet orders
   },
   addOrder(cost, order) {
     this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
+    this.orders.push(order); // Add new order and calculate new value of current balance
   },
   // Change code above this line
 };
--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,20 @@
 'use strict';
 class StringBuilder {
+  #value;
   constructor(initialValue) {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
   getValue() {
-    return this.value;
+    return this.#value;
   }
   padEnd(str) {
-    this.value += str;
+    this.#value = `${this.#value}${str}`;
   }
   padStart(str) {
-    this.value = str + this.value;
+    this.#value = `${str}${this.#value}`;
   }
   padBoth(str) {
-    this.value = str + this.value + str;
+    this.#value = `${str}${this.#value}${str}`;
   }
 }
 
